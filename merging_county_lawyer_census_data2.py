d31717eaf49e45cc627e9f3b1f872573dcd704fd
cwigington3 <cwigington3.github.io>
2017-07-03 11:47:45 -0400

    better shape file

--- merging_county_lawyer_census_data2.py
+++ merging_county_lawyer_census_data2.py
@@ -11,32 +11,35 @@ Created on Mon Mar  6 15:15:26 2017
 @author: wigington
 """
 
-in2 = open("County_Lawyer_and_census_data.json")
+in2 = open("GA_Counties/ga_counties.json")
+next = in2.readline()
 next = in2.readline()
 countiesDict = {}
 while next != "":
     next = next.strip('\n')
+    next = next.strip('\t')
+	coordinatesline = next
     next = next.split(',')
-    totalPop = next[26]
-    CountyName = next[0]
+    CountyName = next[2]
     CountyName = CountyName.split(':')[1]
     CountyName = CountyName.strip(' ')    
     CountyName = CountyName.strip('"')
-    countiesDict[CountyName] = totalPop
+    coordinates = coordinatesline[coordinatesline.find("coordinates")-1:] 
+	countiesDict[CountyName] = coordinates
     next = in2.readline()
 #counties = counties.sort()
-in1 = open("Georgia_Data.js")
+in1 = open("us-states.js")
 #in2 = open("County_Lawyer_and_census_data.json")
 County = in1.readline()
 County = in1.readline()
-out = open("Georgia_Data_1.js",'w')
+out = open("lowres_Georgia_Counties.js",'w')
 out.write('''var statesData = {"type":"FeatureCollection","features":[\n''')
 while County != " ]}\n":
     countyName = County[County.find("name")+7:County.find("Active")-3]
     #countiesDict[countyName]['organization']
-    countyInfoMatchPrefix = County[0:County.find("Shorthand_FIPS")-1]
-    countyInfoMatchSuffix = County[County.find("Shorthand_FIPS")-1:County.find("\n")+2]
-    fullLine = countyInfoMatchPrefix + str(countiesDict[countyName]) +", " + countyInfoMatchSuffix
+    countyInfoMatchPrefix = County[0:County.find("coordinates")-1]
+    #countyInfoMatchSuffix = County[County.find("Shorthand_FIPS")-1:County.find("\n")+2]
+    fullLine = countyInfoMatchPrefix + countiesDict[countyName] + '\n'
     out.write(fullLine)
     County = in1.readline()
 out.write(''']}''')
